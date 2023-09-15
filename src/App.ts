33b8b822c1c9cf9f7eb7598646c24ac79693132e
Amresh Maurya <amresh.terminal@gmail.com>
2023-09-15 12:07:53 +0530

    testing eslint

--- src/App.ts
+++
@@ -1,9 +0,0 @@
-const App = () => {
-  return (
-    <div>
-      <h1 className="text-center text-white bg-slate-950">
-        Hello</h1>
-    </div>
-  )
-}
-export default App
---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-center text-white bg-slate-950"> Hello </h1>
+        </div>
+    );
+};
+export default App;
