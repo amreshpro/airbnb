c47289d4793bd1e50d19bd01fd6cbfd1b50d42c5
Amresh Maurya <amresh.terminal@gmail.com>
2024-04-14 22:48:17 +0530

    fix issue

--- src/Testing.tsx
+++ src/Testing.tsx
@@ -1,6 +1,5 @@
 import { Footer } from './components';
 import CardContainer from './components/card/CardContainer';
-import FooterTabNavigation from './components/footer/FooterTabNavigation';
 import NavFooterMid from './components/footer/NavFooterMid';
 import Navbar from './components/header/Navbar';
 import TopNavList from './components/header/TopNavList';
